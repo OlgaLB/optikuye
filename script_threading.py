c8314bf8f47855348f56a11a280128291b3c68e5
OlgaLB <60224128+OlgaLB@users.noreply.github.com>
2020-05-11 15:30:34 +0200

    Update script_threading.py

--- script_threading.py
+++ script_threading.py
@@ -4,12 +4,12 @@ import time
 import requests
 import threading
 
+
 def get_single_page(url):
     with requests.Session().get(url) as response:
         print(f"Read {len(response.content)} from {url}")
 
 if __name__ == "__main__":
-
     websites = [ "https://www.google.de/", ] * 10
 
     # with Thread
